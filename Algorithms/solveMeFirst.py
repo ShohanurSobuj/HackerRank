684c1dc55862873af36d62f2915fa7d560011f8b
Shohanur Islam Sobuj <shohanursobuj@gmail.com>
2022-03-24 00:21:44 +0600

    solveMeFirst problem Sln

---
+++ Algorithms/solveMeFirst.py
@@ -0,0 +1,12 @@
+# Source : https://leetcode.com/problems/palindrome-number
+# Author : Md. Shohanur Islam Sobuj
+#  Time : 03/24/2022 12:20
+
+def solveMeFirst(a,b):
+  return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print (res)
+
